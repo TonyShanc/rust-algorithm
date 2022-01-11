446587b9672c09857152dae202d5860aaac33db3
TonyShanc <shanah01@foxmail.com>
2022-01-11 21:00:10 +0800

    feat(sort): quick sort

---
+++ src/sort/quick_sort.rs
@@ -0,0 +1,36 @@
+pub fn partition<T: PartialOrd>(arr: &mut [T], low: isize, high: isize) -> isize {
+    let pivot = high as usize;
+    let mut i = low - 1;
+    let mut j = high;
+
+    loop {
+        i += 1;
+        while arr[i as usize] < arr[pivot] {
+            i += 1;
+        }
+        j -= 1;
+        while j >= 0 && arr[j as usize] > arr[pivot] {
+            j -= 1;
+        }
+        if i >= j {
+            break;
+        }else {
+            arr.swap(i as usize, j as usize)
+        }
+    }
+    arr.swap(i as usize, pivot as usize);
+    i
+}
+
+fn _quick_sort<T: Ord>(arr: &mut [T], low: isize, high: isize) {
+    if low < high {
+        let p = partition(arr, low, high);
+        _quick_sort(arr, low, p - 1);
+        _quick_sort(arr, p + 1, high);
+    }
+}
+
+pub fn quick_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    _quick_sort(arr, 0, (len - 1) as isize);
+}
